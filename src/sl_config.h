c19d26c4048839491858482ab282e7e37859b286
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-03-26 12:23:14 -0600

    cmake variables update

--- src/sl_config.h
+++ src/sl_config.h
@@ -6,8 +6,8 @@
 #ifndef SL_CONFIG_H_
 #define SL_CONFIG_H_
 
-#define SL_CONFIG_VERSION_STRING "1.1"
-#define SL_CONFIG_VERSION_BCD 0x101
+#define SL_CONFIG_VERSION_STRING "1.2"
+#define SL_CONFIG_VERSION_BCD 0x102
 #define SL_CONFIG_DOCUMENT_ID "2Cd5njdQwfvvxB1yJTK1"
 #define SL_CONFIG_NAME "i2ctool"
 #define SL_CONFIG_TYPE "app"
