5d3532597efd6f4479349548269d2a3d5a7ee077
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2021-07-14 17:02:18 -0600

    master updates

--- src/sl_config.h
+++ src/sl_config.h
@@ -9,6 +9,7 @@
 #define SL_CONFIG_VERSION_STRING "1.2"
 #define SL_CONFIG_VERSION_BCD 0x102
 #define SL_CONFIG_DOCUMENT_ID "2Cd5njdQwfvvxB1yJTK1"
+#define SL_CONFIG_TEAM_ID ""
 #define SL_CONFIG_NAME "i2ctool"
 #define SL_CONFIG_TYPE "app"
 #define SL_CONFIG_PUBLISHER "Stratify Labs, Inc"
